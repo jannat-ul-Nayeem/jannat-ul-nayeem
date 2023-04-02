5e1902a20611977ddb53b13582acad1065edfb03
Your Name <c223293@urad.iiuc.ac.bd>
2023-04-02 23:50:56 +0600

    tawrat

---
+++ README.md
@@ -0,0 +1,1 @@
+# jannat-ul-nayeem
---
+++ class1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    printf("jannat\n");
+    printf("nayeem\n");
+    printf("tawrat\n");
+    printf("ruhi\n");
+    printf("trp\n");
+/*int value,number;
+    scanf("%d",&value);
+    if(value<0)
+    {
+        number=value * -1;
+        value=number;
+    }
+    printf("%d",value);*/
+    return 0;
+}
